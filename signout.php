821173fb5177a4b4a7f4174e9ca523509f8d043a
Winona Tong <cutewoebot@gmail.com>
2013-05-01 01:22:27 -0700

    make sign out page pretty

--- signout.php
+++ signout.php
@@ -18,4 +18,50 @@ require_once 'config.php';
 session_start();
 session_destroy();
 ?>
-You have been signed out. <a href="<?= $base_url ?>">Sign back in</a>
+<!doctype html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Glassagram</title>
+    <link href="./static/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <style>
+        .button-icon { max-width: 75px; }
+        .tile {
+            border-left: 1px solid #444;
+            padding: 5px;
+            list-style: none;
+        }
+
+    </style>
+</head>
+<body>
+<div class="navbar navbar-inverse navbar-fixed-top">
+    <div class="navbar-inner">
+        <div class="container">
+            <a class="brand" href="#">Glassagram</a>
+            <div class="nav-collapse collapse">
+                <form class="navbar-form pull-right" action="<?= $base_url ?>" method="post">
+                    <button type="submit" class="btn">Sign in</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+
+    <div class="hero-unit">
+        <h2>You have been signed out. </h2>
+        <?php if ($message != "") { ?>
+            <span class="label label-warning">Message: <?= $message ?> </span>
+        <?php } ?>
+        <div style="clear:both;"></div>
+    </div>
+
+</div>
+
+<script
+    src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script src="/static/bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
